2d56d87b1e71287da2339c4b5e1126f68cd45688
Sam <32462530+samlowe106@users.noreply.github.com>
2019-06-08 22:37:47 -0400

    Added an unfinished file-reading method to the Map class

---
+++ Omnipath/Objects/Map Objects/Map.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Omnipath.Objects.Map_Objects
+{
+    /// <summary>
+    /// A playable area loaded from a file
+    /// </summary>
+    class Map
+    {
+        public Map(string fileName)
+        {
+            Load(fileName);
+        }
+
+        /// <summary>
+        /// Loads map from a file
+        /// </summary>
+        public void Load(string fileName)
+        {
+            // Establish the stream and reader as null
+            FileStream inStream = null;
+            BinaryReader reader = null;
+
+            // Try reading from the file
+            try
+            {
+                inStream = File.OpenRead(fileName);
+                reader = new BinaryReader(inStream);
+
+                // TODO: READ DATA HERE
+                
+            }
+            // Catch any exceptions
+            catch (Exception e)
+            {
+                // TODO: PRINT ERROR MESSAGES
+            }
+            // Finally, close the file (if it was successfully opened)
+            finally
+            {
+                if (reader != null)
+                {
+                    reader.Close();
+                }
+            }
+        }
+
+    }
+}
