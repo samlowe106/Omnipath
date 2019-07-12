4b4081f273e9da22fdb581cb591667a7b0eb5ee7
Sam <32462530+samlowe106@users.noreply.github.com>
2019-07-11 23:46:45 -0400

    Added PlayerAction enum which represents different actions the player can take (will be mapped to button presses to allow remappable controls)

---
+++ Omnipath/Enumerations/PlayerAction.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Omnipath
+{
+    /// <summary>
+    /// Actions that the player can take while the game is open; associated with key presses
+    /// </summary>
+    enum PlayerAction
+    {
+        Pause = 0,
+        MainAttack = 1,
+        SecondaryAttack = 2
+    }
+}
