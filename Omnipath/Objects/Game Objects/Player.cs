52ec9e58fb281b13bfb1413e15087039a3d37acb
Sam <32462530+samlowe106@users.noreply.github.com>
2019-07-11 22:17:58 -0400

    Added basic structure for input processing (not yet working)

--- Omnipath/Objects/Game Objects/Player.cs
+++ Omnipath/Objects/Game Objects/Player.cs
@@ -41,13 +41,41 @@ namespace Omnipath
 
         }
 
+        public void Update(KeyboardState currentkbState, KeyboardState previouskbState,
+            MouseState currentMouseState, MouseState previousMouseState)
+        {
+            ProcessInput(currentkbState, previouskbState, currentMouseState, previousMouseState);
+
+
+
+            
+            base.Update();
+        }
+
         /// <summary>
         /// Processes the player's inputs and acts accordingly
         /// </summary>
         /// <param name="currentState"></param>
-        public void ProcessInput(KeyboardState currentState)
+        public void ProcessInput(KeyboardState currentkbState, KeyboardState previouskbState,
+            MouseState currentMouseState, MouseState previousMouseState)
         {
-
+            if (currentkbState.IsKeyDown(Keys.W) /*&&  aboveTilePassable */)
+            {
+                
+            }
+            if (currentkbState.IsKeyDown(Keys.A) /*&&  westTilePassable */)
+            {
+                
+            }
+            if (currentkbState.IsKeyDown(Keys.S) /*&&  southTilePassable */)
+            {
+                
+            }
+            if (currentkbState.IsKeyDown(Keys.D) /*&&  eastTilePassable */)
+            {
+                
+            }
+            
         }
     }
 }
