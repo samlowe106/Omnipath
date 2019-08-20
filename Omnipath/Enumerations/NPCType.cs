5bd0fe6adedf15e4a34a4dc2024fd80d67526e33
Sam Lowe <samlowe106@gmail.com>
2019-08-20 11:26:05 -0400

    Added enumeration for NPCs

---
+++ Omnipath/Enumerations/NPCType.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Omnipath
+{
+    /// <summary>
+    /// An enumeration for NPCs and enemies
+    /// NPCs have a unique ID, enemies typically don't
+    /// </summary>
+    public enum NPCType
+    {
+        // 0 is reserved for "no NPC here"
+        PlaceHolderEnemy = 1,
+        SecondPlaceholderEnemy = 2
+    }
+}
