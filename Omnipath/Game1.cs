ea1716701c923290311dba889527af06827ab63a
Sam Lowe <samlowe106@gmail.com>
2019-08-20 20:15:39 -0400

    Added list of textures

--- Omnipath/Game1.cs
+++ Omnipath/Game1.cs
@@ -23,6 +23,7 @@ namespace Omnipath
         Texture2D playerTexture;
 
         Dictionary<Keys, PlayerAction> controlMapping;
+        List<Texture2D> textures;
 
         KeyboardState currentkbState;
         KeyboardState previouskbState;
